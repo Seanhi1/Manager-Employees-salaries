a3a8b24ce59ec9afab0d0808abe479443f7e3c9a
seanhicks <Seannerbond@gmail.com>
2023-06-06 15:59:01 -0700

    part 1

---
+++ employee.js
@@ -0,0 +1,12 @@
+class Employee{
+    constructor(name, salary, title, manager) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager;
+        if(!manager) {
+            this.manager = null;
+        }
+    }
+}
+module.exports = Employee;
