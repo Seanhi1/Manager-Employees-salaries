23ae756b2ac14ee8c63e416784c87d471bbd6ecd
seanhicks <Seannerbond@gmail.com>
2023-06-06 16:33:30 -0700

    not much change

--- manager.js
+++ manager.js
@@ -10,7 +10,20 @@ class Manager extends Employee {
         this.employees.push(employee)
         }
     }
+    // const splinter = new Manager('Splinter', 100000, 'Sensai');
+    // console.log('Before: ', splinter);
 
+    // const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+    // const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+    // const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+    // const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+    // splinter.addEmployee(leo);
+    // splinter.addEmployee(mikey);
+    // splinter.addEmployee(donnie);
+    // splinter.addEmployee(raph);
+
+    // console.log('After: ', splinter);
 module.exports = Manager
 
 // const splinter = new Manager('Splinter', 100000, 'Sensei');
